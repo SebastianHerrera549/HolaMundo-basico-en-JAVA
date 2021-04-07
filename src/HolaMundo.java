7f74597a88350689568ba260f2e56f82c1d1d52d
Jhoan Sebastian Herrera Molina <sebastian.herrera.molina@hotmail.com>
2021-04-07 08:17:07 -0500

    Update HolaMundo.java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,6 +3,6 @@ public class HolaMundo
 {
    public static void main(String []args)
    {
-    System.out.println("Hola Mundo");
+    System.out.println("Hola Mundo");// esta funcion imprime en pantalla lo que este entre comillas dobles en este caso HOLA MUNDO
     }
 }
